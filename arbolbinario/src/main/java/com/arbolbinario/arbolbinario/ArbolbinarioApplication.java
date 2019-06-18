aa377b5801677ae9cc320308be32c23c06ded98c
ricardoavendano <51935170+ricardoavendano@users.noreply.github.com>
2019-06-17 19:36:26 -0500

    Add files via upload

---
+++ arbolbinario/src/main/java/com/arbolbinario/arbolbinario/ArbolbinarioApplication.java
@@ -0,0 +1,13 @@
+package com.arbolbinario.arbolbinario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbolbinarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbolbinarioApplication.class, args);
+	}
+
+}
